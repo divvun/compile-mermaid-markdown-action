9d174f2ff9e2f5736ebfb1b3f1467d5eee1ae3c7
Sjur Moshagen <sjurnm@mac.com>
2022-02-21 15:42:37 +0200

    Kor ligg filene hen?

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,8 @@ set -euox pipefail
 MMDC_EXTRA_ARGS="${MMDC_EXTRA_ARGS:-}"
 
 function main {
-  ls -laR /node_modules /mmdc
+  ls -la /
+  ls -la /mmdc
   printf "Using MMDC version in node_modules/: %s\n" "$(/node_modules/.bin/mmdc -V)"
   printf "Using MMDC version in mmdc/: %s\n" "$(/mmdc/mmdc -V)"
 
