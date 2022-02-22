f1c16069990844e8861c0a3f154b1c0afce598ec
Sjur Moshagen <sjurnm@mac.com>
2022-02-22 13:01:09 +0200

    Betre find

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,7 +24,7 @@ MMDC_EXTRA_ARGS="${MMDC_EXTRA_ARGS:-}"
 function main {
   ls -la /
   ls -la /mmdc
-  find . -name 'mmdc'
+  echo "Kva har eg funne: $(find . -name 'mmdc' | tr ' ' '\n' )"
   printf "Using MMDC version in node_modules/: %s\n" "$(/node_modules/.bin/mmdc -V)"
   printf "Using MMDC version in mmdc/: %s\n" "$(/mmdc/mmdc -V)"
 
