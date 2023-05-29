10178269a73cb666d5a94e35593ddd65515df891
Sjur N Moshagen <sjurnm@mac.com>
2023-05-29 18:14:07 +0300

    realpath is not working — debug galore

--- entrypoint.sh
+++ entrypoint.sh
@@ -134,6 +134,7 @@ function c_md_mermaid {
     ls -l
     printf "Input dir ${input_dir}"
     printf "Target filename: ${output_path}/${dasherized}-${block_count}.${output_file_type}"
+    realpath --help
     sleep 2
 
     # Compute relative path from the markdown to the tmp_dir
